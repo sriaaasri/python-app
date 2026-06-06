7a0f75d0a45aeece1425c95fd0102dd42cf8e0ea
sri_satya <psatyachowdary2001@gmail.com>
2026-06-06 21:47:06 +0530

    First project commit

---
+++ test_app.py
@@ -0,0 +1,14 @@
+#test_app.py
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.testing = True
+    return app.test_client()
+
+def test_home(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.data == b"Hello, Jenkins Pipeline with Python!"
+    
