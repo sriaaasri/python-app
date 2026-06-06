3f2756a09dcb61b2e54f92351e8ba9ebb44ac4b4
sri_satya <91309599+sriaaasri@users.noreply.github.com>
2026-06-07 00:02:38 +0530

    modified the app response

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Jenkins Pipeline with Python!"
+    return "Hello, Jenkins Pipeline with Python! Happy Learning^^"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3001)
